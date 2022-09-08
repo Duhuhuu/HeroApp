4044a45dbc23dba37dec14c27ddf5b2244465b3d
Duhuhuu <ignisxmizu@gmail.com>
2022-09-07 19:26:32 -0600

    Actu de Rutas

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,36 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Navbar } from "../../ui";
+import { DcPage, MarvelPage } from "../pages";
+import { HeroPage } from "../pages/HeroPage";
+import { SearchPages } from "../pages/SearchPages";
+
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar />
+          <div className="container" >
+            <Routes>
+
+                <Route path="/marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+
+                <Route path="search" element={<SearchPages />} />
+                <Route path="hero" element={<HeroPage />} />
+
+
+                <Route path="/" element={<Navigate to="/marvel" />} />
+            </Routes>   
+
+        </div>
+
+
+
+
+
+
+
+
+    </>
+  )
+}
